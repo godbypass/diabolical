a9bd0ba074fbbea13ea1e978117854bae3516214
ImLegiitXD <116777906+ImLegiitXD@users.noreply.github.com>
2025-09-12 17:16:17 -0400

    holy sh

---
+++ src/main/java/Start.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.util.Arrays;
+
+import net.minecraft.client.main.Main;
+
+public final class Start
+{
+    public static void main(final String[] args)
+    {
+        final File minecraft_loc = getMinecraftDir();
+        final File assets_loc = new File(minecraft_loc, "assets");
+
+        Main.main(concat(new String[] {
+                "--version", "Steve",
+                "--accessToken", "0",
+                "--gameDir", minecraft_loc.exists() ? minecraft_loc.toString() : "",
+                "--assetsDir", assets_loc.exists() ? assets_loc.toString() : "assets",
+                "--assetIndex", "1.8",
+                "--userProperties", "{}"}, args));
+    }
+
+    private static File getMinecraftDir() {
+        final String fileLocation;
+        final net.minecraft.util.Util.EnumOS operatingSystem = net.minecraft.util.Util.getOSType();
+        switch (operatingSystem) {
+            case WINDOWS:
+                fileLocation = (System.getenv("APPDATA") + "/.minecraft");
+                break;
+            case OSX:
+                fileLocation = (System.getProperty("user.home") + "/Library/Application Support/minecraft");
+                break;
+            default: // for linux & others
+                fileLocation = (System.getProperty("user.home") + "/.minecraft");
+                break;
+        }
+        return new File(fileLocation);
+    }
+
+    public static <T> T[] concat(final T[] first, final T[] second)
+    {
+        final T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+}
